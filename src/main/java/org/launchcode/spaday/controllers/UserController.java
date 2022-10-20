9deb8c1f74f35859e9ab5cd253f5ea837540b5fe
Emily Hart <emilykhart726@gmail.com>
2022-10-19 20:46:43 -0500

    studio part2

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,20 +3,34 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("add")
-    public String displayAddUserForm(){
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
-    @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
-        return "/user/index";
+    @PostMapping
+    public String processAddUserForm(@Valid @ModelAttribute User user, Errors errors) {
+
+        if (!errors.hasErrors()) {
+            return "user/index";
+        }
+        else {
+//            model.addAttribute("passwordMatchError", "Passwords do not match");
+            return "user/add";
+        }
+
     }
+
 
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,38 +1,67 @@
 package org.launchcode.spaday.models;
 
-public class User {
+import javax.validation.constraints.*;
 
+public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6)
     private String password;
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
+    @NotNull(message = "Passwords do not match.")
+    @NotBlank
+    private String verify;
+
+    public User() {
+
     }
 
-    public String getUsername() {
-        return username;
+    public User(String username, String email, String password, String verify) {
+            this();
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.verify = verify;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+        public void setUsername(String username) {
+            this.username = username;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+            checkPassword(password, this.verify);
+        }
+
+        public String getVerify() {
+            return verify;
+        }
+
+        public void setVerify(String verify) {
+            this.verify = verify;
+            checkPassword(this.password, verify);
+        }
+
+        private void checkPassword(String password, String verify){
+            if(password != null && verify != null && !password.equals(verify)){
+                this.verify = null;
+            }
+        }
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
